f5f3d7152f78ebd0a1a331369357cbea5aff22f1
SuuduBABA <sowayouba1@gmail.com>
2022-05-10 14:37:55 +0200

    Footer add

--- wp-content/themes/inspiro/template-parts/footer/site-info.php
+++ wp-content/themes/inspiro/template-parts/footer/site-info.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays footer site info
  *
@@ -11,21 +12,21 @@
 ?>
 <div class="site-info">
 	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	if (function_exists('the_privacy_policy_link')) {
+		the_privacy_policy_link('', '<span role="separator" aria-hidden="true"></span>');
 	}
 	?>
 	<span class="copyright">
 		<span>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'inspiro' ) ); ?>" target="_blank">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'inspiro')); ?>" target="_blank">
 				<?php
 				/* translators: %s: WordPress trademark */
-				printf( esc_html__( 'Powered by %s', 'inspiro' ), 'WordPress' );
+				printf(esc_html__('Powered by %s', 'inspiro'), 'WordPress');
 				?>
 			</a>
 		</span>
 		<span>
-			<?php esc_html_e( 'Inspiro WordPress Theme by', 'inspiro' ); ?> <a href="<?php echo 'https://www.wpzoom.com/'; ?>" target="_blank" rel="nofollow">WPZOOM</a>
+			<?php esc_html_e('Parc régional de Nice', 'Griot'); ?> <a href="<?php echo 'https://www.wpzoom.com/'; ?>" target="_blank" rel="nofollow">WPZOOM</a>
 		</span>
 	</span>
 </div><!-- .site-info -->
